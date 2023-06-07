898e21e6b156f1ca082549c55ac962b711fccbd3
Davor Telisman <davortelisman@gmail.com>
2023-06-07 18:44:06 +0200

    Create database connection Django and MySQL database: set settings to mysql create config for values in are connection with database: USER,PASSWORD..

--- django_and_pytest/settings.py
+++ django_and_pytest/settings.py
@@ -65,9 +65,12 @@ WSGI_APPLICATION = 'django_and_pytest.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        'OPTIONS': {
+            'read_default_file': 'shopping_db.cnf',
+        },
+
     }
 }
 
