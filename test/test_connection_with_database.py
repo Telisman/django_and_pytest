cfbc705398ca1e781151a332d63edeb20258129e
Davor Telisman <davortelisman@gmail.com>
2023-06-09 11:13:54 +0200

    Testing fixture in are test_ex1.py file

--- test/test_connection_with_database.py
+++ test/test_connection_with_database.py
@@ -3,14 +3,14 @@ from django.db import connection
 
 
 @pytest.mark.django_db
-def test_database_connection(capfd):
+def test_database_connection():
     with connection.cursor() as cursor:
         cursor.execute("SELECT 1")
         result = cursor.fetchone()
-
     assert result[0] == 1
     print("Database connection is successful!")
 
-    # Retrieve the captured stdout and check if the print statement was output
-    captured = capfd.readouterr()
-    assert "Database connection is successful!" in captured.out
+
+# Run pytest with the -s option to enable output capture
+pytest.main(["-s"])
+
